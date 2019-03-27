3a34b2ffff3bafc598858c1202596497d4ee3157
Adam <17008675@e007m07.istic.univ-rennes1.fr>
2019-03-27 15:59:40 +0100

    Copy from echo

---
+++ src/istic/pr/socket/tcp/nom/ClientTCP.java
@@ -0,0 +1,129 @@
+//...
+
+package istic.pr.socket.tcp.nom;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.*;
+
+public class ClientTCP {
+
+	public static void main(String[] args) throws IOException, IOException {
+		// créer une socket client
+
+		String ip = "";
+		while (!checkip(ip)) {
+			System.out.println(" IP du serveur ?");
+			ip = lireMessageAuClavier();
+			// verif format TODO
+		}
+		int port = 9999;// pour si on fait le mode "en une saisie"
+
+		/*
+		 * System.out.println("Port ? "); // verif format. possibilité de mettre
+		 * 0.0.0.0:0 pour l'adresse complete, TODO Scanner sc = new Scanner(System.in);
+		 * port = sc.nextInt();
+		 */
+		Socket socket = new Socket(InetAddress.getByName(ip), port);
+		// créer reader et writer associés
+		// sc.close();
+		BufferedReader in = creerReader(socket);
+		PrintWriter out = creerPrinter(socket);
+
+		// Tant que le mot «fin» n’est pas lu sur le clavier,
+		// Lire un message au clavier
+		// envoyer le message au serveur
+		// recevoir et afficher la réponse du serveur
+
+		String msg = "";
+		while (!msg.equalsIgnoreCase("fin")) {
+			msg = lireMessageAuClavier();
+			envoyerMessage(out, msg);
+			System.out.println(recevoirMessage(in));
+		}
+		// envoyer le message au serveur
+
+		// recevoir et afficher la réponse du serveur
+	
+	end(socket, in, out);
+	}
+
+/**
+ * Vérifie le format de l'ip
+ * @param ip string de l'ip en ipv4 ex:"127.0.0.1"
+ * @return l'ip est valide
+ */
+	private static boolean checkip(String ip) {
+		 try {
+		        if ( ip == null || ip.isEmpty() ) {
+		            return false;
+		        }
+		        String[] parts = ip.split( "\\." );
+		        if ( parts.length != 4 ) {
+		            return false;
+		        }
+		        for ( String s : parts ) {
+		            int i = Integer.parseInt( s );
+		            if ( (i < 0) || (i > 255) ) {
+		                return false;
+		            }
+		        }
+		        if ( ip.endsWith(".") ) {
+		            return false;
+		        }
+		        return true;
+		    } catch (NumberFormatException nfe) {
+		        return false;
+		    }
+		 
+	}
+
+	public static String lireMessageAuClavier() throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("> ");
+		String str = sc.nextLine();
+		// sc.close();
+		return str;
+	}
+
+	public static BufferedReader creerReader(Socket socketVersUnClient) throws IOException {
+		// crÃ©Ã© un BufferedReader associÃ© Ã  la Socket
+		return new BufferedReader(new InputStreamReader(socketVersUnClient.getInputStream()));
+	}
+
+	public static PrintWriter creerPrinter(Socket socketVersUnClient) throws IOException {
+		// crÃ©Ã© un PrintWriter associÃ© Ã  la Socket
+		return new PrintWriter(new OutputStreamWriter(socketVersUnClient.getOutputStream()));
+	}
+
+	public static String recevoirMessage(BufferedReader reader) throws IOException {
+		return reader.readLine();
+		// identique serveur
+	}
+
+	public static void envoyerMessage(PrintWriter printer, String message) throws IOException {
+		printer.println(message);
+		printer.flush();
+		// Envoyer le message vers le client
+	}
+	public static void end (Socket socket, BufferedReader in, PrintWriter out) {
+		
+		try {
+			if (in != null)
+				in.close();
+			if (out != null)
+				out.close();
+			if (socket != null)
+				socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/istic/pr/socket/tcp/nom/ServeurTCP.java
@@ -0,0 +1,71 @@
+package istic.pr.socket.tcp.nom;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+
+public class ServeurTCP {
+
+    public static void main(String[] args) throws IOException {
+
+        //Attente des connexions sur le port 9999
+    	int portEcoute = 9999;
+    	ServerSocket socketServeur = new ServerSocket(portEcoute);
+    	System.out.println("Server open with ip: " + socketServeur.getInetAddress() + " port: " + socketServeur.getLocalPort());
+    	
+    	//Dans une boucle, pour chaque socket clientes, appeler traiterSocketCliente
+    	while (true) {
+    		Socket socketVersUnClient = socketServeur.accept();
+    		traiterSocketCliente(socketVersUnClient);
+    	}
+    }
+
+    public static void traiterSocketCliente(Socket socketVersUnClient) throws IOException {
+        //Créer printer et reader
+    	PrintWriter printer = creerPrinter(socketVersUnClient);
+    	BufferedReader reader = creerReader(socketVersUnClient);
+    	
+    	String msg;
+        //Tant qu’il y’a un message à lire via recevoirMessage
+    	while((msg = recevoirMessage(reader)) != null) {
+    		System.out.println("Msg received: " + msg);
+    		//Envoyer message au client via envoyerMessage
+    		envoyerMessage(printer, msg);
+    	}
+
+        //Si plus de ligne à lire fermer socket cliente
+    	socketVersUnClient.close();
+    }
+
+    public static BufferedReader creerReader(Socket socketVersUnClient)
+    throws IOException {
+        //créé un BufferedReader associé à la Socket
+    	return new BufferedReader(new InputStreamReader(socketVersUnClient.getInputStream()));
+    }
+
+    public static PrintWriter creerPrinter(Socket socketVersUnClient) throws
+    IOException {
+        //créé un PrintWriter associé à la Socket
+    	return new PrintWriter(new OutputStreamWriter(socketVersUnClient.getOutputStream()));
+    }
+
+    public static String recevoirMessage(BufferedReader reader) throws
+    IOException {
+        //Récupérer une ligne
+    	return reader.readLine();
+        //Retourner la ligne lue ou null si aucune ligne à lire.
+    }
+
+    public static void envoyerMessage(PrintWriter printer, String message)
+    throws IOException {
+    	printer.println(message);
+    	printer.flush();
+        //Envoyer le message vers le client
+    }
+
+}
