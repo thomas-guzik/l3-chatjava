1ccfe0c0ef0243c932d6304ccb88e1646c147e8a
thomas-guzik <thomas.guzik@laposte.net>
2019-03-27 14:35:41 +0100

    derniere modif

--- src/istic/pr/socket/tcp/echo/ServeurTCP.java
+++ src/istic/pr/socket/tcp/echo/ServeurTCP.java
@@ -4,42 +4,37 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 
 public class ServeurTCP {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //Attente des connexions sur le port 9999
     	int portEcoute = 9999;
-    	try {
-	    	ServerSocket socketServeur = new ServerSocket(portEcoute);
-	    	
-	    	while (true) {
-	    		Socket socketVersUnClient = socketServeur.accept();
-	    		traiterSocketCliente(socketVersUnClient);
-	    		
-	    	}
-
-
-        //Dans une boucle, pour chaque socket clientes, appeler traiterSocketCliente
+    	ServerSocket socketServeur = new ServerSocket(portEcoute);
     	
-    	} catch (IOException e) { //Traitement des exceptions
-    		e.printStackTrace();
+    	//Dans une boucle, pour chaque socket clientes, appeler traiterSocketCliente
+    	while (true) {
+    		Socket socketVersUnClient = socketServeur.accept();
+    		traiterSocketCliente(socketVersUnClient);
     	}
-    	
     }
 
-    public static void traiterSocketCliente(Socket socketVersUnClient) {
+    public static void traiterSocketCliente(Socket socketVersUnClient) throws IOException {
         //Créer printer et reader
-    	creerPrinter(socketVersUnClient);
-    	creerReader(socketVersUnClient);
+    	PrintWriter printer = creerPrinter(socketVersUnClient);
+    	BufferedReader reader = creerReader(socketVersUnClient);
     	
-
+    	String msg;
         //Tant qu’il y’a un message à lire via recevoirMessage
-
-        //Envoyer message au client via envoyerMessage
+    	while((msg = recevoirMessage(socketVersUnClient)) != null) {
+    		//Envoyer message au client via envoyerMessage
+    		envoyerMessage(printer, msg);
+    	}
 
         //Si plus de ligne à lire fermer socket cliente
     }
@@ -47,21 +42,26 @@ public class ServeurTCP {
     public static BufferedReader creerReader(Socket socketVersUnClient)
     throws IOException {
         //créé un BufferedReader associé à la Socket
+    	return new BufferedReader(new InputStreamReader(socketVersUnClient.getInputStream()));
     }
 
     public static PrintWriter creerPrinter(Socket socketVersUnClient) throws
     IOException {
         //créé un PrintWriter associé à la Socket
+    	return new PrintWriter(new OutputStreamWriter(socketVersUnClient.getOutputStream()));
     }
 
-    public static String recevoirMessage(BufferedReader reader) throws
+    public static String recevoirMessage(Socket socketVersUnClient) throws
     IOException {
         //Récupérer une ligne
+    	return socketVersUnClient.readLine();
         //Retourner la ligne lue ou null si aucune ligne à lire.
     }
 
     public static void envoyerMessage(PrintWriter printer, String message)
     throws IOException {
+    	printer.println(message);
+    	printer.flush();
         //Envoyer le message vers le client
     }
 
